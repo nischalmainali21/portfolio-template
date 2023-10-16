245ae8f73ee3b0b8bacb1ad3358275d185e14e6a
Nischal Mainali <nischalmainali21@gmail.com>
2023-10-16 17:04:46 +0545

    intro > layout completed     To be updated with real info and links.

--- components/intro.tsx
+++ components/intro.tsx
@@ -3,6 +3,10 @@
 import Image from "next/image";
 import React from "react";
 import { motion } from "framer-motion";
+import Link from "next/link";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import { FaGithubSquare } from "react-icons/fa";
 
 function Intro() {
   return (
@@ -37,7 +41,53 @@ function Intro() {
         placeat eveniet aut.
       </motion.p>
 
-      <div>{/* button links */}</div>
+      <motion.div
+        className="flex flex-col sm:flex-row items-center justify-center gap-4 px-4 text-lg font-medium "
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.1,
+        }}
+      >
+        {/* button links */}
+        <Link
+          href="#contact"
+          className=" group flex items-center bg-gray-900 text-white px-7 py-3 gap-2 rounded-full
+          outline-none focus:scale-110 hover:scale-110 hover:bg-gray-900 active:scale-105 transition"
+        >
+          Contact Me{" "}
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+        </Link>
+
+        <a
+          className="group flex items-center bg-gray-200 px-7 py-3 gap-2 rounded-full 
+        outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10"
+          href="/CV.pdf"
+          download={true}
+        >
+          Donwload CV{" "}
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+        </a>
+
+        {/* Change the linkedin and github link */}
+        <a
+          className="flex items-center bg-zinc-50 p-4 gap-2 rounded-full 
+        outline-none focus:scale-125 hover:scale-125 hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10"
+          href="https://youtu.be/dQw4w9WgXcQ"
+          target="_blank"
+        >
+          <BsLinkedin />
+        </a>
+
+        <a
+          className="flex items-center bg-zinc-50 p-4 gap-2 rounded-full 
+        outline-none focus:scale-125 hover:scale-125 active:scale-105 transition cursor-pointer hover:text-gray-950 border border-black/10"
+          href="https://github.com/nischalmainali21"
+          target="_blank"
+        >
+          <FaGithubSquare />
+        </a>
+      </motion.div>
     </section>
   );
 }
