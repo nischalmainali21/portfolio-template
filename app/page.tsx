2ce91b62b2735ea18927086eab1be00288ed26f6
Nischal Mainali <nischalmainali21@gmail.com>
2023-10-17 11:13:59 +0545

    Added about section to the page     Major issue: All animation loads at the start of page loading     , animation needs to load according to the section that is in view.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+import About from "@/components/about";
 import Intro from "@/components/intro";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="pt-28 sm:pt-36 flex flex-col items-center px-4">
+    <main className="pt-28 sm:pt-36 flex flex-col items-center">
       <Intro />
+      <About />
     </main>
   );
 }
