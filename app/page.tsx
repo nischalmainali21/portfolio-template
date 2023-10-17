90d570946c4d40d71c6104da1e6a511c435da583
Nischal Mainali <nischalmainali21@gmail.com>
2023-10-17 15:06:28 +0545

    Added projects sectoion to pages.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import About from "@/components/about";
 import Intro from "@/components/intro";
+import Projects from "@/components/projects";
 import Image from "next/image";
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <main className="pt-28 sm:pt-36 flex flex-col items-center">
       <Intro />
       <About />
+      <Projects />
     </main>
   );
 }
