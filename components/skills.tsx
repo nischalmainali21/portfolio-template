433de49e9692a2030205c8a5a8c3f485fbd1fe4b
Nischal Mainali <nischalmainali21@gmail.com>
2023-10-18 20:41:11 +0545

    Finished general layout of skills section     Better Animation code update requqired.

--- components/skills.tsx
+++ components/skills.tsx
@@ -2,6 +2,23 @@
 import React from "react";
 import SectionHeading from "./section-heading";
 import { useSectionInView } from "@/lib/hooks";
+import { skillsData } from "@/lib/data";
+import { delay, motion, Variants } from "framer-motion";
+
+const fadeInAnimationVaraints = {
+  offScreen: {
+    opacity: 0,
+    y: 100,
+  },
+  onScreen: (index: number) => ({
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: 0.05 * index,
+      duration: 0.2,
+    },
+  }),
+};
 
 export default function Skills() {
   const { ref } = useSectionInView({ sectionName: "Skills", threshold: 0.3 });
@@ -10,9 +27,27 @@ export default function Skills() {
     <section
       className="bg-[#f5f8fc] w-full px-4 pb-16 sm:pb-20 sm:px-32 scroll-mt-16"
       ref={ref}
+      id="skills"
     >
       <div>
         <SectionHeading>My Skills</SectionHeading>
+        <div className="flex items-center justify-center">
+          <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800 max-w-[53rem]">
+            {skillsData.map((skill, index) => (
+              <motion.li
+                key={index}
+                className="bg-white border border-black/[0.1] rounded-xl py-3 px-5"
+                variants={fadeInAnimationVaraints}
+                initial="offScreen"
+                whileInView="onScreen"
+                viewport={{ once: true }}
+                custom={index}
+              >
+                {skill}
+              </motion.li>
+            ))}
+          </ul>
+        </div>
       </div>
     </section>
   );
