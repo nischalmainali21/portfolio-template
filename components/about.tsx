e69cd3aee9848bf5200636ed84905821e7e7649a
Nischal Mainali <nischalmainali21@gmail.com>
2023-10-18 09:03:59 +0545

    Updated animation to play only wheninViewport     Needs to be Updated to work when using ref

--- components/about.tsx
+++ components/about.tsx
@@ -1,22 +1,57 @@
 "use client";
 
 import Image from "next/image";
-import React from "react";
-import { motion } from "framer-motion";
+import React, { useRef } from "react";
+import { motion, Variants } from "framer-motion";
 import SectionHeading from "./section-heading";
 
+const ImageVariants: Variants = {
+  offScreen: {
+    opacity: 0,
+    x: 100,
+  },
+  onScreen: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      tyep: "tween",
+      duration: 0.8,
+    },
+  },
+};
+
+const DescVariants: Variants = {
+  offScreen: {
+    opacity: 0,
+    x: -100,
+  },
+  onScreen: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 0.8,
+    },
+  },
+};
+
 export default function About() {
+  // const scrollRef = useRef<HTMLElement>(null); did not work for now
   return (
-    <section className="bg-[#f5f8fc] w-full px-4 pb-16 sm:pb-20 sm:px-32">
+    <section
+      // ref={scrollRef}
+      className="bg-[#f5f8fc] w-full px-4 pb-16 sm:pb-20 sm:px-32"
+    >
       <div className="flex flex-col">
         <SectionHeading>About Me</SectionHeading>
         <div className="flex flex-col sm:flex-row-reverse sm:flex-grow sm:justify-between sm:gap-5 md:gap-4">
           <div className="mb-6 sm:mb-0">
             {/* image  maybe a black and white image*/}
             <motion.div
-              initial={{ opacity: 0, x: 100 }}
-              animate={{ opacity: 1, x: 0 }}
-              transition={{ type: "tween", duration: 0.5 }}
+              variants={ImageVariants}
+              initial="offScreen"
+              whileInView="onScreen"
+              viewport={{ once: true, amount: 0.5 }}
+              // viewport={{ root: scrollRef, amount: 1 }}//did not work
             >
               <Image
                 src="/nischal_portrait.jpg"
@@ -29,15 +64,16 @@ export default function About() {
             </motion.div>
           </div>
           <motion.div
-            initial={{ opacity: 0, x: -100 }}
-            animate={{ opacity: 1, x: 0 }}
-            transition={{ delay: 0.1, duration: 0.5 }}
+            variants={DescVariants}
+            initial="offScreen"
+            whileInView="onScreen"
+            viewport={{ once: true, amount: 0.5 }}
             className="sm:max-w-[45%]"
           >
             <div className="flex flex-col">
               {/* some paragraphs */}
               <span className="text-xl mb-6 sm:text-3xl sm:mb-10">
-                Hi, I&apos;m
+                Hi, I&apos;m{" "}
                 <span className="font-bold text-2xl sm:text-4xl">
                   Nischal Mainali
                 </span>
