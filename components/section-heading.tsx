df3fba63e690ee9fb604ef6ee7b6a761450b62ed
Nischal Mainali <nischalmainali21@gmail.com>
2023-10-19 13:56:15 +0545

    pixel changes

--- components/section-heading.tsx
+++ components/section-heading.tsx
@@ -6,7 +6,7 @@ type SectionHeadingProps = {
 
 export default function SectionHeading({ children }: SectionHeadingProps) {
   return (
-    <h2 className="font-medium capitalize text-4xl mb-6 sm:text-5xl sm:mb-10 mt-8">
+    <h2 className="font-medium capitalize text-4xl mb-6 sm:text-5xl sm:mb-10 mt-8 ml-2 sm:ml-0">
       {children}
     </h2>
   );
