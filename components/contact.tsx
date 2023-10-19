742d8ea7383b1b71e2737f7aea8f5fe46ea50525
Nischal Mainali <nischalmainali21@gmail.com>
2023-10-19 13:56:29 +0545

    contact section completed.

--- components/contact.tsx
+++ components/contact.tsx
@@ -9,37 +9,37 @@ export default function Contact() {
   const { ref } = useSectionInView({ sectionName: "Contact", threshold: 0.3 });
   return (
     <section
-      // ref={scrollRef}
       ref={ref}
       className="bg-[#f5f8fc] w-full px-4 pb-16 sm:pb-20 sm:px-32 scroll-mt-16"
       id="contact"
     >
-      <div>
+      <div className="flex flex-col">
         <SectionHeading>Contact Me</SectionHeading>
-      </div>
-      <div className="flex items-center justify-center w-full">
-        <div className="flex flex-col sm:flex-row items-center justify-center gap-10">
-          <div className="flex gap-6 sm:gap-4 items-center justify-center bg-white/60 border border-black/[0.05] rounded-xl p-6">
-            <div className="w-16 sm:w-20 bg-neutral-100 rounded-full h-16 sm:h-20 flex items-center justify-center">
-              <BsFillTelephoneFill className="" />
+        <div className="flex items-center justify-center">
+          <div className="flex flex-col sm:flex-row gap-8 items-center justify-center w-full sm:max-w-[53rem]">
+            <div className="flex gap-4 sm:gap-10 items-center justify-center bg-white/60 border border-black/[0.05] rounded-xl w-[85%] sm:w-[50%] h-40 p-4">
+              <div className="bg-neutral-100 rounded-full p-4 sm:p-6">
+                <BsFillTelephoneFill className="scale-105 sm:scale-125" />
+              </div>
+              <div>
+                <h3 className="font-semibold capitalize text-xl sm:text-2xl text-gray-900">
+                  Phone
+                </h3>
+                <p className="font-normal text-gray-800">+977 9811122233</p>
+              </div>
             </div>
-            <div className="flex flex-col items-center justify-center gap-">
-              <h3 className="font-semibold capitalize text-2xl text-gray-900">
-                Phone
-              </h3>
-              <p className="font-normal text-gray-800">+977 9811111111</p>
-            </div>
-          </div>
-
-          <div className="flex gap-6 sm:gap-4 items-center justify-center bg-white/60 border border-black/[0.05] rounded-xl sm:p-[6%] p-6">
-            <div className="w-16 sm:w-20 bg-neutral-100 rounded-full h-16 sm:h-20 flex items-center justify-center">
-              <MdEmail className="" />
-            </div>
-            <div className="flex flex-col items-center justify-center gap-4">
-              <h3 className="font-semibold capitalize text-2xl text-gray-900">
-                Email
-              </h3>
-              <p className="font-normal text-gray-800">some@gmail.com</p>
+            <div className="flex gap-4 sm:gap-10 items-center justify-center bg-white/60 border border-black/[0.05] rounded-xl w-[85%] sm:w-[50%] h-40 p-4">
+              <div className="bg-neutral-100 rounded-full p-4 sm:p-6">
+                <MdEmail className="scale-105 sm:scale-125" />
+              </div>
+              <div>
+                <h3 className="font-semibold capitalize text-xl sm:text-2xl text-gray-900">
+                  Email
+                </h3>
+                <p className="font-normal text-gray-800">
+                  nischalmainali21@gmail.com
+                </p>
+              </div>
             </div>
           </div>
         </div>
